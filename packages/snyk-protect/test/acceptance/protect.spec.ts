d8d710986ebd62c89ebd1230411551fcca6501b0
maxjeffos <44034094+maxjeffos@users.noreply.github.com>
2021-04-28 15:25:31 -0400

    test: organize tests

--- packages/snyk-protect/test/acceptance/protect.spec.ts
+++ packages/snyk-protect/test/acceptance/protect.spec.ts
@@ -22,42 +22,44 @@ describe('@snyk/protect', () => {
     jest.restoreAllMocks();
   });
 
-  it('works for project with a single patchable module', async () => {
-    const fixture = 'single-patchable-module';
-    const fixtureFolder = path.join(__dirname, '../fixtures', fixture);
-    const modulePath = path.join(tempFolder, fixture);
-    const targeFilePath = path.join(
-      modulePath,
-      'node_modules/nyc/node_modules/lodash/lodash.js',
-    );
-
-    await fse.copy(fixtureFolder, modulePath);
-    await protect(modulePath);
-
-    const actualPatchedFileContents = fs.readFileSync(targeFilePath, 'utf-8');
-    expect(actualPatchedFileContents).toMatchSnapshot();
-  });
-
-  it('works for project with multiple patchable modules', async () => {
-    const fixture = 'multiple-matching-paths';
-    const fixtureFolder = path.join(__dirname, '../fixtures', fixture);
-    const modulePath = path.join(tempFolder, fixture);
-    const targeFilePath1 = path.join(
-      modulePath,
-      'node_modules/nyc/node_modules/lodash/lodash.js',
-    );
-    const targeFilePath2 = path.join(
-      modulePath,
-      'node_modules/lodash/lodash.js',
-    );
-
-    await fse.copy(fixtureFolder, modulePath);
-    await protect(modulePath);
-
-    const actualPatchedFileContents = fs.readFileSync(targeFilePath1, 'utf-8');
-    expect(actualPatchedFileContents).toMatchSnapshot();
-    const actualPatchedFileContents2 = fs.readFileSync(targeFilePath2, 'utf-8');
-    expect(actualPatchedFileContents2).toMatchSnapshot();
+  describe('applies patch(es)', () => {
+    it('works for project with a single patchable module', async () => {
+      const fixture = 'single-patchable-module';
+      const fixtureFolder = path.join(__dirname, '../fixtures', fixture);
+      const modulePath = path.join(tempFolder, fixture);
+      const targeFilePath = path.join(
+        modulePath,
+        'node_modules/nyc/node_modules/lodash/lodash.js',
+      );
+  
+      await fse.copy(fixtureFolder, modulePath);
+      await protect(modulePath);
+  
+      const actualPatchedFileContents = fs.readFileSync(targeFilePath, 'utf-8');
+      expect(actualPatchedFileContents).toMatchSnapshot();
+    });
+  
+    it('works for project with multiple patchable modules', async () => {
+      const fixture = 'multiple-matching-paths';
+      const fixtureFolder = path.join(__dirname, '../fixtures', fixture);
+      const modulePath = path.join(tempFolder, fixture);
+      const targeFilePath1 = path.join(
+        modulePath,
+        'node_modules/nyc/node_modules/lodash/lodash.js',
+      );
+      const targeFilePath2 = path.join(
+        modulePath,
+        'node_modules/lodash/lodash.js',
+      );
+  
+      await fse.copy(fixtureFolder, modulePath);
+      await protect(modulePath);
+  
+      const actualPatchedFileContents = fs.readFileSync(targeFilePath1, 'utf-8');
+      expect(actualPatchedFileContents).toMatchSnapshot();
+      const actualPatchedFileContents2 = fs.readFileSync(targeFilePath2, 'utf-8');
+      expect(actualPatchedFileContents2).toMatchSnapshot();
+    });
   });
 
   describe('does not apply any patches and does not fail', () => {
